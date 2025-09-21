69ef2b144cb9fbaeac6c14a2503e41295a189da5
Thewise-man <ugorjiwisdom156@gmail.com>
2025-09-21 22:29:27 +0100

    updated responsiveness

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
-const menuIcon = document.getElementById("menuIcon");
+/* const menuIcon = document.getElementById("menuIcon");
 const navLinks = document.getElementById("navLinks");
 
 menuIcon.addEventListener("click", () => {
   navLinks.classList.toggle("active");
-});
+}); */
+
+let menuList = document.getElementById("menuList");
+        menuList.style.maxHeight = "0px";
+
+        function toggleMenu() {
+          if (menuList.style.maxHeight == "0px") {
+            menuList.style.maxHeight = "300px";
+          } else {
+            menuList.style.maxHeight = "0px";
+          }
+        }
